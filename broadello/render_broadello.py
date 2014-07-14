2b4884872a691584546717716f9d30825c7276cb
Hussein Elgridly <hussein@broadinstitute.org>
2014-07-13 22:44:11 -0400

    Tweaked until I got it working. Made sure rss validated for itunes. Fixed issues with dates and paths.

--- broadello/render_broadello.py
+++ broadello/render_broadello.py
@@ -1,30 +1,26 @@
-from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
 import os
+import email
+from bs4 import BeautifulSoup
 
-env = Environment(loader=PackageLoader('broadello', 'templates'))
-
+env = Environment(loader=FileSystemLoader('templates'))
 
 import episodes
-
-
-#app = Flask(__name__)
-
-
-#if __name__ == '__main__':
-#    app.run()
-
-with open("test.html","w") as out:
-    template = env.get_template("index.template.html")
-    out.writelines( template.render( episodes=episodes.episodes))
 
 url="http://www.broadinstitute.org/~hussein/broadello/"
 path="/home/unix/hussein/public_html/broadello/"
 
-with open("rss.html", "w") as out:
+with open(path+"index.html","w") as out:
+    template = env.get_template("index.template.html")
+    out.writelines( template.render(episodes=episodes.episodes) )
+
+with open(path+"rss.xml", "w") as out:
     template = env.get_template("rss.template.html")
     for ep in episodes.episodes:
         ep["url"] = url + ep["path"]
         ep["size"] = os.path.getsize(path + ep["path"])
+        ep["date"] = email.Utils.formatdate(os.path.getctime(path + ep["path"]))[:-3] + "500"
+        ep["description"] = BeautifulSoup(ep["description"]).get_text()
 
     print episodes.episodes
 
